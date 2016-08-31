fbc84dfddbdd00d7989be3acc304bbdef2e00779
solarfl4re <solarfl4re@users.noreply.github.com>
2016-08-31 13:44:33 -0800

    Update blogroll links

--- pelicanconf.py
+++ pelicanconf.py
@@ -25,8 +25,7 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         ('Jinja2', 'http://jinja.pocoo.org/'),)
 
 # MENUITEMS = (
 #     ('About', 'https://github.com/'),
